037db01cc8c128600c955d31f38ee0129bccd17f
olaf <juliendupdupdup@gmail.com>
2024-09-22 18:14:11 +0100

    code cleanded from signedOut function

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,24 +1,15 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { SignedIn, useUser } from '@clerk/clerk-expo'
+import { Text } from 'react-native'
 
 export default function Page() {
   const { user } = useUser()
 
   return (
-    <View>
+    <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
-      <SignedOut>
-        <Link href="/sign-in">
-          <Text>Sign In</Text>
-        </Link>
-        <Link href="/sign-up">
-          <Text>Sign Up</Text>
-        </Link>
-      </SignedOut>
-    </View>
+    </SafeAreaView>
   )
 }
