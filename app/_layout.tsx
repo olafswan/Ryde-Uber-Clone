f873dc76ffa4d973c4b6e370d86d17343b3f1fe2
olaf <juliendupdupdup@gmail.com>
2024-10-07 15:03:59 +0100

    minor fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,7 +20,7 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-LogBox.ignoreLogs(["Warning: ..."]);
+LogBox.ignoreLogs(["Clerk:"]);
 
 export default function RootLayout() {
   const [loaded] = useFonts({
